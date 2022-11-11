0006bea6e89e37eb8758df8d9c6aca28688abc38
NguyenHoangDuy <t0p1starb0y@gmail.com>
2022-11-11 21:26:57 +0700

    root

---
+++ src/component/nav/Nav.jsx
@@ -0,0 +1,25 @@
+
+function Nav() {
+    return(
+        <div className={'top-main-nav'}>
+            <div>
+                <img src="../../assets/image-logo.jpg" alt=""/>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>CSS</li>
+                    <li><a href="#"></a>DOM</li>
+                    <li><a href="#"></a>React</li>
+                </ul>
+            </nav>
+            <div>
+                <a href="#">Theme</a>
+            </div>
+            <div>
+                <a href="#">Login</a>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
---
+++ src/container/header/Header.jsx
@@ -0,0 +1,13 @@
+import {Nav} from '../../component/index';
+
+function Header() {
+
+    return (
+        <header>
+            <Nav/>
+            header
+        </header>
+    )
+}
+
+export default Header;
